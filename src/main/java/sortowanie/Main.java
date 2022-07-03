449c89129c6ea9ec36fa0bdd2232516c6e447af7
Dakoog <taktoja@spoko.pl>
2022-07-03 22:59:44 +0200

    Initial commit

---
+++ src/main/java/sortowanie/Main.java
@@ -0,0 +1,52 @@
+package sortowanie;
+
+import javax.lang.model.util.AbstractElementVisitor14;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> studentsName = new ArrayList<>();
+        studentsName.add("Dorota");
+        studentsName.add("Iris");
+        studentsName.add("Anastazja");
+        studentsName.add("Doman");
+        studentsName.add("Zenobiusz");
+        studentsName.add("Xawery");
+
+        System.out.println("\n---------------");
+        System.out.println(studentsName);
+        for (String name : studentsName) {
+            System.out.println(name);
+        }
+
+        //Strings sorting A -> Z by Collections.sort()
+        System.out.println("\nSorting students name A -> Z.");
+       /* Collections.sort(studentsName);
+        System.out.println(studentsName);
+        for (String sortName : studentsName) {
+            System.out.println(sortName);
+        }*/
+        // String sorting A -> Z by Comparator
+        studentsName.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        System.out.println(studentsName);
+        for (String sortName : studentsName) {
+            System.out.println(sortName);
+        }
+        System.out.println("\nSorting students name Z -> A.");
+        studentsName.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        System.out.println(studentsName);
+        for (String sortName : studentsName) {
+            System.out.println(sortName);
+        }
+    }
+}
