2710c5559c9c01ac594b160baa7627f4c9bf159c
Dakoog <taktoja@spoko.pl>
2022-07-04 09:39:26 +0200

    adding Person object to package sorting.

---
+++ src/main/java/sortowanie/Person.java
@@ -0,0 +1,23 @@
+package sortowanie;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    /*public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }*/
+
+}
