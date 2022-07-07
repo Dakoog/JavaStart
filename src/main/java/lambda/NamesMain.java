124272ed184f9264d329fe267f520b3ab2eed12e
Dakoog <taktoja@spoko.pl>
2022-07-07 20:31:30 +0200

    adding NamesMain application to lambda package.

---
+++ src/main/java/lambda/NamesMain.java
@@ -0,0 +1,14 @@
+package lambda;
+
+public class NamesMain {
+
+    public static void main(String[] args) {
+        ListOfNames namesList = new ListOfNames();
+        System.out.println();
+        namesList.firstName();
+        namesList.men();
+        namesList.women();
+
+
+    }
+}
